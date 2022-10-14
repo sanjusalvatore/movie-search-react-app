a014c7036cf0de80d9e014cbdc4a0ad5ae9d924a
Shubh Chudasama <65857597+c-shubh@users.noreply.github.com>
2022-10-14 15:10:42 +0530

    fix: Search by pressing Enter key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
 import { useEffect, useState } from "react";
@@ -20,21 +19,28 @@ const App = () => {
     searchMovie("game");
   }, []);
 
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    searchMovie(searchTerm);
+  }
+
   return (
     <div className="app">
       <h1>MovieCenter </h1>
-      <div className="search">
+      <form className="search" onSubmit={handleFormSubmit}>
         <input
           placeholder="Enter the Movie"
           value={searchTerm}
           onChange={(e) => setSearchterm(e.target.value)}
         />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovie(searchTerm)}
-        />
-      </div>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <circle cx={10} cy={10} r={7}></circle>
+   <line x1={21} y1={21} x2={15} y2={15}></line>
+</svg>
+        </button>
+      </form>
       {Movie?.length > 0 ? (
         <div className="container">
           {Movie.map((movie) => (
