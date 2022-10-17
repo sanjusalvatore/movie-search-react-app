0185c49c6a2fe1db0e67a6d3416d845c30b841fb
Shubh Chudasama <65857597+c-shubh@users.noreply.github.com>
2022-10-17 21:22:37 +0530

    Use tailwind.css

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const MovieCard = ({ data }) => {
   return (
-    <div className="movie">
-      <div>
+    <div className="movie group relative w-[310px] h-[460px] m-6 rounded-lg overflow-hidden shadow-lg transition duration-300 hover:shadow-[0px_30px_18px_-8px_rgba(0,0,0,0.1)] hover:scale-105">
+      <div className="absolute p-4 opacity-0 text-orange-200 group-hover:opacity-100">
         <p>{data.Year}</p>
       </div>
-      <div>
+      <div className="w-full h-full group-hover:opacity-30">
         <img
           alt={`${data.Title} poster`}
           src={
@@ -14,12 +14,17 @@ const MovieCard = ({ data }) => {
               ? data.Poster
               : "https://via.placeholder.com/400"
           }
+          className="w-full h-full"
         />
       </div>
 
-      <div>
-        <span>{data.Type}</span>
-        <h3>{data.Title}</h3>
+      <div className="absolute inset-x-0 bottom-0 bg-neutral-700 pt-4 px-6 pb-6 group-hover:bg-transparent">
+        <span className="uppercase text-xs tracking-[.16em] font-medium text-zinc-100">
+          {data.Type}
+        </span>
+        <h3 className="text-orange-200 mt-1.5 font-bold text-xl">
+          {data.Title}
+        </h3>
       </div>
     </div>
   );
